661c20da4c71ce9a6fdd441298f44d7fdd4142bb
Wilson <wilsoncoutinh@gmail.com>
2024-08-31 20:51:33 -0300

    "Updated imports from 'next/navigation' to 'next/router' in multiple files, and modified tsconfig.json to update lib, paths, and include/exclude settings."

--- MediApp/medi-app/src/app/appointment/create/page.tsx
+++ MediApp/medi-app/src/app/appointment/create/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/router';
 import React, { useState, useEffect } from "react";
 
 export default function AppointmentCreate() {
--- MediApp/medi-app/src/app/doctor/create/page.tsx
+++ MediApp/medi-app/src/app/doctor/create/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/router';
 import React, { useState } from "react";
 
 export default function DoctorCreate() {
